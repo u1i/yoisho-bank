d5aab32ce637160ccf3f497d3526a3d37291462b
Uli Hitzel <uli.hitzel@gmail.com>
2020-11-06 11:40:04 +0800

    changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,6 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
@@ -37,11 +32,6 @@ const Footer = class extends React.Component {
                     <li>
                       <Link className="navbar-item" to="/products">
                         Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
                       </Link>
                     </li>
                     <li>
