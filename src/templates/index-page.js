1c255bd189b592d7bd4e6ea233c207402bbc7a27
Uli Hitzel <uli.hitzel@gmail.com>
2020-11-06 11:54:48 +0800

    changed

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -54,7 +54,7 @@ export const IndexPageTemplate = ({
           style={{
             boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            backgroundColor: 'rgb(14, 83, 154)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
